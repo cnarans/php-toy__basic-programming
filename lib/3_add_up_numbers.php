2d85acb243c66c703591bdf19071d3e1663b5038
Colin Narans <cnarans@gmail.com>
2016-06-21 16:22:18 -0500

    code working

--- lib/3_add_up_numbers.php
+++ lib/3_add_up_numbers.php
@@ -8,8 +8,14 @@
 
 function addUpNumbers($arr) {
   // Your code goes here.
+	$x = 0;
+	foreach($arr as $number){
+		$x+=$number;
+	}
+	return $x;
 }
 
 // Write your own "tests" below. Refer to the the examples from exercise #1.
-
+$test = array(1,2,3,4,5,6,7,8,9,10);
+echo addUpNumbers($test);
 ?>
--- lib/4_filter_numbers.php
+++ lib/4_filter_numbers.php
@@ -7,9 +7,21 @@
 // Run `bin/check` from the command line to execute the automated tests.
 
 function filterNumbers($arr) {
-  // Your code goes here.
+	$fil = array();
+	foreach($arr as $number){
+		if($number<10){
+			array_push($fil, $number);
+		}
+		else{
+		}
+	}
+	return $fil;
 }
 
-// Write your own "tests" below. Refer to the the examples from exercise #1.
+$test = array(2,5,55,33,9,10,11,12,-3);
+$test = filterNumbers($test);
+foreach($test as $x){
+	echo $x;
+}
 
 ?>
