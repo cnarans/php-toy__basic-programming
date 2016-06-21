cbf4fa73a71a172f9ec51cbd3e6e46bf43a898c2
Colin Narans <cnarans@gmail.com>
2016-06-21 16:11:17 -0500

    test

--- lib/2_uppercases_all_letters.php
+++ lib/2_uppercases_all_letters.php
@@ -7,9 +7,29 @@
 // Run `bin/check` from the command line to execute the automated tests.
 
 function uppercasesAllLetters($str) {
-  // Your code goes here.
+  return strtoupper($str);
 }
 
 // Write your own "tests" below. Refer to the the examples from exercise #1.
+if(ctype_upper(uppercasesAllLetters("alllower"))){
+	echo "Success!";
+}
+else{
+	echo "Failure!";
+}
+
+if(ctype_upper(uppercasesAllLetters("ALLUPPER"))){
+	echo "Success!";
+}
+else{
+	echo "Failure!";
+}
+
+if(ctype_upper(uppercasesAllLetters("MixEdCAse"))){
+	echo "Success!";
+}
+else{
+	echo "Failure!";
+}
 
 ?>
